2f662617b62b77323fbc7e1f84255698df363e63
Daniel San <danielgdt88@hotmail.com>
2025-02-17 16:22:12 -0300

    destroy mock with real call

--- src/app/components/countries-flag/countries-flag.component.ts
+++ src/app/components/countries-flag/countries-flag.component.ts
@@ -3,24 +3,25 @@ import { RequestApiService } from '../../services/request-api.service';
 import { IDetailsCountries } from '../../models/detailsCountries.interface';
 import { ChangeThemeColorDirective } from '../../directives/change-theme-color.directive';
 import { ModeThemeService } from '../../services/mode-theme.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-countries-flag',
   standalone: true,
-  imports: [ChangeThemeColorDirective],
+  imports: [CommonModule, ChangeThemeColorDirective],
   templateUrl: './countries-flag.component.html',
   styleUrl: './countries-flag.component.scss'
 })
 export class CountriesFlagComponent implements OnInit{
 
-  listCountries?: IDetailsCountries;
+  listCountries: IDetailsCountries[] = [];
 
   constructor (private readonly _requestApi: RequestApiService, protected readonly _modeService: ModeThemeService) {}
 
   ngOnInit(): void {
     this._requestApi.getCountries().subscribe({
       next: (value) => {
-        this.listCountries = value[0]
+        this.listCountries = value
         console.log(this.listCountries)
       },
       error: (err: any) => {
