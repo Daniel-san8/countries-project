188b2b277631fbd18633d9e10c37433e5362c7df
Daniel San <danielgdt88@hotmail.com>
2025-02-19 23:19:59 -0300

    feat: implementing function for return home

--- src/app/components/detail-countrie/detail-countrie.component.ts
+++ src/app/components/detail-countrie/detail-countrie.component.ts
@@ -3,10 +3,11 @@ import { ModeThemeService } from '../../services/mode-theme.service';
 import { ChangeThemeColorDirective } from '../../directives/change-theme-color.directive';
 import { faArrowLeftLong } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-detail-countrie',
-  imports: [ChangeThemeColorDirective, FontAwesomeModule],
+  imports: [ChangeThemeColorDirective, FontAwesomeModule, RouterLink],
   templateUrl: './detail-countrie.component.html',
   styleUrl: './detail-countrie.component.scss'
 })
